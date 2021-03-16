ecf45ad49e9ed2e1e29fd8bfa78a571be1d7f418
Rejep <rej@MacBook-Pro.local>
2021-03-15 21:12:47 -0600

    Initial commit

---
+++ src/test/java/com/swaglabs/runner/TestRunner.java
@@ -0,0 +1,31 @@
+package com.swaglabs.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        // plugin is for reports
+        plugin = {"pretty", "html:target/site/cucumber-pretty.html"},
+
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+
+        glue = {"com/swaglabs/stepdefinitions"},
+
+        features = {"src/test/resources/features"},
+
+        tags = "@login",
+
+        // checking if the steps are defined or not
+        dryRun = false,
+
+        monochrome = true
+
+
+)
+
+public class TestRunner {
+}
